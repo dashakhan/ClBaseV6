aefad0059734bc2160205b6ef52fbc68813857e0
DashaGKH <keshonti2016@gmail.com>
2024-05-09 19:45:13 -0700

    config and dependencies installation

---
+++ .mocharc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    require: ['@babel/register'],
+    timeout: '5000',
+ }
+
+
+ 
---
+++ test/example.spec.js
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("VERIFY SIMPLE MATH FUNCTIONS", () => {
+  it("verify sum works properly", () => {
+    expect(4 + 4).eq(8);
+  });
+});
