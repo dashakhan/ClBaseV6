cad9a870e785b00eebd73ab5a13bbbe3c30f6fbe
DashaGKH <keshonti2016@gmail.com>
2024-05-15 18:04:55 -0700

    add auth positive

---
+++ test/auth.spec.js
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import {expect} from 'chai'
+
+
+describe('AUTHENTIFICATION',  () => {
+    it('login with valid credentials', async () => {
+         const res =  await request('https://clientbase-server.herokuapp.com/v6/')
+        .post('user/login')
+        .send({
+            email: 'jacksparrow@pirate.com',
+            password: 'Pirate666!'
+        })
+        expect(res.status).to.eq(200)
+        expect(res.body.message).to.eq('Auth success')
+        console.log(res.body);
+    })
+    
+});
