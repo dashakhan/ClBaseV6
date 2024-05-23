c67d49f620e96e51211142f37532f17330622888
dash <89936740+dashakhan@users.noreply.github.com>
2024-05-22 21:08:11 -0400

    add hooks (#2)

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,8 @@
 module.exports = {
     require: ['@babel/register'],
     timeout: '10000',
-    exclude: ['test/exaple.spec.js']
+    exclude: ['test/exaple.spec.js'],
+    file: 'setup/project-setup.js'
  }
 
 
