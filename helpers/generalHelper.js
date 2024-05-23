379c53d373bce4161d5ad5e3bfee9b3501d28393
DashaGKH <keshonti2016@gmail.com>
2024-05-22 18:54:28 -0700

    add general helper

---
+++ helpers/generalHelper.js
@@ -0,0 +1,13 @@
+import request from 'supertest'
+
+
+export function login(email, password){
+    return request(process.env.BASE_URL)
+    .post('user/login')
+    .send({email: email, password: password})
+    //  .send({email, password}) if key and value is the same we can shorten it 
+}
+
+//export default {login} - another way of export
+
+
--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,38 +1,31 @@
-import request from 'supertest'
 import {expect} from 'chai'
-import 'dotenv/config'
+import {login} from '../helpers/generalHelper'
+//import 'dotenv/config'  - used to be here, but since we have it in a file/no need
+//import request from 'supertest' - used to be here as well, but now we moved 'request' into helpers
 
 describe('AUTHENTIFICATION', () => {
-    
+    let res;
     describe('POSITIVE AUTHENTIFICATION',  () => {
-        let res;
         before(async()=>{
-            res = await request(process.env.BASE_URL)
-            .post('user/login')
-            .send({email: process.env.EMAIL, password: process.env.PASSWORD})
+        res = await login(process.env.EMAIL, process.env.PASSWORD)
         })
-
 
         it('verify status code', async () => {
         expect(res.status).to.eq(200)
-        console.log(process.env.TOKEN);
          })
 
-
          it('verify response message', async () => {
-            expect(res.body.message).to.eq('Auth success')
+        expect(res.body.message).to.eq('Auth success')
         })
     
 });
 
 
-
 describe('NEGATIVE AUTHENTIFICATION', () =>{
-    let res;
-    before(async()=>{        
-        res = await request(process.env.BASE_URL)
-        .post('user/login')
-        .send({email: process.env.INVALID_EMAIL, password: process.env.INVALID_PASSWORD})
+    describe('login with invalid email', ()=>{
+
+    before(async() => {        
+        res = await login('', process.env.PASSWORD)
 
     })
     it('verify status code is 400', async ()=>{
@@ -46,5 +39,26 @@ describe('NEGATIVE AUTHENTIFICATION', () =>{
     it('verify response body success - is false', async ()=>{
         expect(res.body.success).to.eq(false)
     })
+})
+
+
+ describe('login with invalid password', ()=>{
+
+    before(async()=>{        
+        res = await login(process.env.EMAIL, '')
+
+    })
+    it('verify status code is 400', async ()=>{
+        expect(res.status).to.eq(400)   
+    })
+
+    it('verify body message - Auth fail ', async ()=>{
+        expect(res.body.message).to.eq('Auth failed')
+    })
+
+    it('verify response body success - is false', async ()=>{
+        expect(res.body.success).to.eq(false)
+    })
+})
 })
 })
