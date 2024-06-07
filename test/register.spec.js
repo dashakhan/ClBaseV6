881416e90bd65c976423c52aa9478061d0b9c56d
dash <89936740+dashakhan@users.noreply.github.com>
2024-06-07 18:47:11 -0400

    new register tests (#7)

---
+++ test/register.spec.js
@@ -0,0 +1,60 @@
+import { expect } from 'chai';
+const chance = require('chance').Chance();
+import { register } from '../helpers/generalHelper';
+import { generateSignUpBody } from '../helpers/constants';
+
+describe.only('REG', () => {
+  let res;
+
+  describe('REGISTER POSITIVE', () => {
+    //console.log(signUpBody);
+
+    before(async () => {
+      res = await register(generateSignUpBody());
+    });
+
+
+    it('verify status code', async () => {
+      expect(res.status).to.eq(201);
+    });
+
+    it('verify response message', async () => {
+      expect(res.body.message).contain('User created');
+    });
+  });
+
+
+
+  describe('REGISTER NEGATIVE', () => {
+
+    describe('reg with existing email', () => {
+      const existingEmailData = { ...generateSignUpBody(), email: process.env.EMAIL };
+      
+      before(async () => {
+        res = await register(existingEmailData);
+        //console.log(res.body, '1')
+      });
+
+
+      it('verify status code', () => {
+        expect(res.status).to.eq(409);
+       
+      });
+    });
+
+
+
+    describe('REGISTER WITHOUT FIRST NAME', () =>{
+   
+      before(async() =>{
+        const noFirstNameData = {...generateSignUpBody(), firstName: ''}
+        res = await register(noFirstNameData)
+        console.log(res.body, '2')
+      })
+
+      it('verify status code', () =>{
+        expect(res.status).to.eq(404)
+      })
+    })
+  });
+});
