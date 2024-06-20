23e41af549612fe91355c2d5e81caddd86df4028
DashaGKH <keshonti2016@gmail.com>
2024-06-19 17:30:30 -0700

    reporter added

--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,17 @@ module.exports = {
     require: ['@babel/register'],
     timeout: '10000',
     exclude: ['test/exaple.spec.js'],
-    file: 'setup/project-setup.js'
+    file: 'setup/project-setup.js',
+    reporter: "mochawesome",
+    reporterOptions: [
+          
+          'json=false',
+          'quiet=true',
+          'reportFileName=updatedReport',
+          'reportDir=MyReports'
+          //'reportFilename=[status]_[datetime]updatedReport'
+    ]
+        
  }
 
 
